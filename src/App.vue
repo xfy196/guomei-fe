eaaf078ae245b340edf8f21d4076e0eb05797074
xfy196 <xfy196@outlook.com>
2020-07-06 23:20:27 +0800

    头部搜索组件和nav列表组件，轮播图组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    hello world
+  <div id="app">
+    <Cupboard></Cupboard>
   </div>
 </template>
 
 <script>
+import Cupboard from "@/cupboard/Home"
 export default {
-  
+  components : {
+    Cupboard
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-
 </style>
---
+++ src/components/cupboard/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <HomeTop></HomeTop>
+    <HomeNav>
+        <template slot="Swipe">
+            <Swipe></Swipe>
+        </template>
+    </HomeNav>
+  </div>
+</template>
+
+<script>
+import Swipe from "@/common/Swipe";
+import HomeTop from "@/Home/HomeTop";
+import HomeNav from "@/Home/HomeNav";
+export default {
+  components: {
+    Swipe,
+    HomeTop,
+    HomeNav
+  }
+};
+</script>
+
+<style>
+</style>
