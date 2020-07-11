def42f9068a036e9bf5f49527502387bc2f1fff1
qinsong <qs603313754@qq.com>
2020-07-11 12:07:21 +0800

    完成消息页

---
+++ src/components/Message/GoBack.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <van-nav-bar
+      title="消息列表"
+      left-text="返回"
+      left-arrow
+      @click-left="onClickLeft"
+      fixed
+      placeholder
+    />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { NavBar } from 'vant';
+Vue.use(NavBar);
+
+export default {
+  methods: {
+    onClickLeft(){
+      console.log(1)
+      this.$router.back();
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/views/Message.vue
+++ src/views/Message.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    消息
+    <!-- 返回功能 -->
+    <GoBack></GoBack>
+    <!-- 消息列表 -->
+    <MessageList></MessageList>
+    <!-- 导购列表 -->
+    <GuideList></GuideList>
   </div>
 </template>
 
 <script>
+import GoBack from '@/Message/GoBack'
+import MessageList from '@/Message/MessageList'
+import GuideList from '@/Message/GuideList'
 export default {
-
+  components: {
+    GoBack,
+    MessageList,
+    GuideList
+  }
 }
 </script>
 
